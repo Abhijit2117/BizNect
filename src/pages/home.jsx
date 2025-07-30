6a429f25967abf4db378a460a74ce9d3e2546de8
abhishek19kahar <abhishekgithub19@gmail.com>
2025-07-30 22:16:35 +0530

    pushing frontend on git

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='w-full h-screen bg-black flex justify-center items-center flex-col p-5 relative text-white'>
+      home hello
+    </div>
+  );
+}
+
+export default Home;
