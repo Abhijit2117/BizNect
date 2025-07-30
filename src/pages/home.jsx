ce040b9465d89c87166b082c51ed370198b6b47d
abhishek19kahar <abhishekgithub19@gmail.com>
2025-07-30 22:44:32 +0530

    added some lines

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Home() {
   return (
     <div className='w-full h-screen bg-black flex justify-center items-center flex-col p-5 relative text-white'>
-      home hello
+      home hello ho
     </div>
   );
 }
